6aeb6d7941e45fca39c62d451db8f6389fac9deb
youxia <hxg2021@gmail.com>
2023-01-18 15:25:38 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:hxg2050/hxg.git main:gh-pages
 
 cd -
