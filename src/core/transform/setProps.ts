15e8cac104f7927d0180d0a7d995682b62611ef1
youxia <hxg2021@gmail.com>
2023-02-03 13:29:33 +0800

    批量快速设置对象属性

---
+++ src/core/transform/setProps.ts
@@ -0,0 +1,19 @@
+
+type Props<T> = {
+    [P in keyof T]?: Props<T[P]>;
+};
+/**
+ * 快速设置
+ * @param obj 
+ * @param props 
+ */
+export function setProps<T>(obj: T, props: Props<T>) {
+	for (let key in props) {
+		const value = props[key];
+		if (typeof value === 'object') {
+			setProps(obj[key], value!);
+			continue;
+		}
+		obj[key] = value as any;
+	}
+}
