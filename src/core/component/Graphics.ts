11ba74908278034b1992b01f9c235092f8471f1b
youxia <hxg2021@gmail.com>
2023-04-03 12:45:57 +0800

    完善注释

--- src/core/component/Graphics.ts
+++ src/core/component/Graphics.ts
@@ -3,8 +3,6 @@ import { BaseContainer } from "./BaseContainer";
 
 /**
  * 图形绘制
- * 1、边框/线条
- * 2、闭合情况/填充
  */
 export class Graphics extends BaseContainer { // extends Component
 
@@ -26,10 +24,15 @@ export class Graphics extends BaseContainer { // extends Component
         this.node.size.emitter.on('change', this.toRedraw, this);
     }
 
-    toRedraw() {
+    private toRedraw() {
         this.redraw = true;
     }
 
+    /**
+     * 新增一条绘制指令
+     * @param name 指令名
+     * @param args 指令参数
+     */
     pushTask(name: string, ...args: (string | number)[]) {
         this.tasks.push({
             action: name,
@@ -39,42 +42,42 @@ export class Graphics extends BaseContainer { // extends Component
     }
 
     /**
-     * 清空画布
+     * 清空画布（清空操作指令历史）
      */
-    clear() { //x: number, y: number, width: number, height: number
-        // this.pushTask('clear', ...arguments);
+    clear() {
         this.tasks.length = 0;
     }
     /**
-     * 填充颜色
+     * 开启新的绘制
      */
     begin() {
         this.pushTask('begin');
     }
 
     /**
-     * 闭合形状
+     * 闭合路径，会将起始点与结束点用直线连接
      */
     closePath() {
         this.pushTask('closePath');
     }
 
     /**
-     * 结束本次绘制
+     * 填充颜色
+     * @param color 颜色
      */
     fill(color: string) {
         this.pushTask('fill', ...arguments);
     }
     /**
-     * 画线
-     * @param color 
+     * 设置边框或线条
+     * @param color 颜色
      */
     stroke(color: string, width: number = 1) {
         this.pushTask('stroke', ...arguments);
     }
 
     /**
-     * 
+     * 移动起始点
      * @param point 移动到
      */
     moveTo(x: number, y: number) {
@@ -82,15 +85,21 @@ export class Graphics extends BaseContainer { // extends Component
     }
 
     /**
-     * 直线到
+     * 绘制一条直线到某点
+     * @param x x坐标
+     * @param y y坐标
      */
     lineTo(x: number, y: number) {
         this.pushTask('lineTo', ...arguments);
     }
 
     /**
-     * 绘制一个矩形
-     * 可带圆角
+     * 绘制一个矩形，可带圆角
+     * @param x 起始位置x坐标
+     * @param y 起始位置y坐标
+     * @param width 宽
+     * @param height 高
+     * @param radius 圆角大小
      */
     drawRect(x: number, y: number, width: number, height: number, radius: number = 0) {
         /**
@@ -116,7 +125,12 @@ export class Graphics extends BaseContainer { // extends Component
     }
 
     /**
-     * 画圆弧
+     * 绘制圆弧
+     * @param x 圆心位置x坐标
+     * @param y 圆心位置y坐标
+     * @param radians 半径
+     * @param startAngle 圆弧起始位置
+     * @param endAngle 圆弧结束位置
      */
     arc(x: number, y: number, radians: number, startAngle: number, endAngle: number) {
         this.pushTask('arc', ...arguments);
@@ -124,6 +138,9 @@ export class Graphics extends BaseContainer { // extends Component
 
     /**
      * 绘制一个圆
+     * @param x 圆心位置x坐标
+     * @param y 圆心位置y坐标
+     * @param radians 半径
      */
     drawCircle(x: number, y: number, radians: number) {
         this.arc(x , y, radians, 0, 2 * Math.PI);
@@ -131,6 +148,14 @@ export class Graphics extends BaseContainer { // extends Component
 
     /**
      * 绘制一个椭圆
+     * @param x 椭圆圆心的 x 轴坐标。
+     * @param x 椭圆圆心的 y 轴坐标。
+     * @param radiusX 椭圆长轴的半径。
+     * @param radiusY 椭圆短轴的半径。
+     * @param rotation 椭圆的旋转角度，以弧度表示 (非角度度数)。
+     * @param startAngle 将要绘制的起始点角度，从 x 轴测量，以弧度表示 (非角度度数)。
+     * @param endAngle 椭圆将要绘制的结束点角度，以弧度表示 (非角度度数)。
+     * @param anticlockwise Boolean 选项，如果为 true，逆时针方向绘制椭圆（逆时针），反之顺时针方向绘制。
      */
     ellipse(x: number, y: number, radiusX: number, radiusY: number, rotation: number, startAngle: number, endAngle: number, anticlockwise: boolean = false) {
         this.pushTask('ellipse', ...arguments);
@@ -138,6 +163,10 @@ export class Graphics extends BaseContainer { // extends Component
 
     /**
      * 绘制一个椭圆
+     * @param x 椭圆圆心的 x 轴坐标。
+     * @param x 椭圆圆心的 y 轴坐标。
+     * @param radiusX 椭圆长轴的半径。
+     * @param radiusY 椭圆短轴的半径。
      */
     drawEllipse(x: number, y: number, radiusX: number, radiusY: number) {
         this.ellipse(x, y, radiusX, radiusY, 0, 0, 2 * Math.PI);
@@ -145,6 +174,7 @@ export class Graphics extends BaseContainer { // extends Component
 
     /**
      * 绘制一个多边形
+     * @param points 多边形每个点的坐标([x1, y2, x2, y2, x3, y3, ...])
      */
     drawPolygon(points: number[]) {
         if (points.length < 6) {
--- src/core/component/Rect.ts
+++ src/core/component/Rect.ts
@@ -2,10 +2,35 @@ import { BaseContainer } from "./BaseContainer";
 import { Component } from "./Component";
 import { Graphics } from "./Graphics";
 
+/**
+ * 矩形绘制，必须挂载Graphics节点上才能生效
+ * ```ts
+ * const node = app.stage.addChild(new Transform(Graphics));
+ * node.position.set(100, 100);
+ * node.size.set(100, 100);
+ * const rect = node.addComponent(Rect)!;
+ * rect.radius = 10;
+ * rect.fill = '#0000FF';
+ * rect.storke = '#ff0000';
+ * rect.storkeWidth = 3;
+ * ```
+ */
 export class Rect extends Component {
+    /**
+     * 圆角大小
+     */
     radius: number = 0;
-    fill: string = '#000000';
+    /**
+     * 填充颜色
+     */
+    fill?: string = '#000000';
+    /**
+     * 边框颜色
+     */
     storke: string = '#FFFFFF';
+    /**
+     * 边框大小
+     */
     storkeWidth: number = 0;
     private graphics?: Graphics;
 
@@ -15,9 +40,13 @@ export class Rect extends Component {
         this.draw();
     }
 
-    draw() {
+    private draw() {
         if (!this.graphics) {
             console.warn('只能挂在`Graphics`节点上');
+            return;
+        }
+
+        if (!this.fill && this.storkeWidth == 0) {
             return;
         }
 
@@ -25,8 +54,8 @@ export class Rect extends Component {
         g.clear();
         g.begin();
         g.drawRect(this.radius / 2, this.radius / 2, this.node.size.x - this.radius, this.node.size.y - this.radius, this.radius);
-        g.fill(this.fill);
-        g.stroke(this.storke, this.storkeWidth);
+        this.fill && g.fill(this.fill);
+        this.storkeWidth > 0 && g.stroke(this.storke, this.storkeWidth);
     }
 
     update(time: number): void {
