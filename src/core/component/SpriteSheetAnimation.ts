c3183531c68c8af16db14e8f92c0d4ccb0a5a5b5
youxia <hxg2021@gmail.com>
2023-04-19 17:50:42 +0800

    完善注释

--- src/core/component/SpriteSheetAnimation.ts
+++ src/core/component/SpriteSheetAnimation.ts
@@ -3,10 +3,42 @@ import { Animation } from "./Animation";
 import { Sprite } from "./Sprite";
 import { SpriteSheet } from "./SpriteSheet";
 
+/**
+ * 适用于texturepacker的动画
+ * 
+ * ```ts
+ * let json = await Resource.load('./example/assets/animations/Samurai/Attack.json');
+ * let img = await Resource.load('./example/assets/animations/Samurai/Attack.png');
+ * // 如果要控制动画大小可以使用父节点包裹的方式控制scale属性
+ * const group = app.stage.addChild(new Transform);
+ * group.scale.set(2);
+ * const node = group.addChild(new Transform(Sprite));
+ * const sprite = node.getComponent(Sprite)!;
+ * const spriteSheet = node.addComponent(SpriteSheet);
+ * const ssa = node.addComponent(SpriteSheetAnimation);
+ * // 设置每秒播放帧数
+ * ssa.fps = 8;
+ * // 设置为无限循环播放
+ * ssa.loop = -1;
+ * const func = () => {
+ * 	console.log('播放到第', ssa.time, '帧');
+ * };
+ * ssa.addFrameAction(2, func, this);
+ * ssa.emitter.on(Animation.Event.ENDED, () => {
+ * 	console.log('单次动画播放完成');
+ * 	ssa.removeFrameAction(2, func, this);
+ * }, this);
+ * spriteSheet.load(json.data, img);
+ * // 需要在下一帧才能正常播放
+ * ticker.once('update', () => {
+ * 	ssa.play('Attack');
+ * }, this);
+ * ```
+ */
 export class SpriteSheetAnimation extends Animation {
 
     /**
-     * 播放的帧速率，按照每秒计算
+     * 播放的帧速率，按照每秒计算，每秒播放的帧数
      */
     fps: number = 60;
 
