8977f39a4bfcece3a3edf5f0d6237fca2aea0ab7
游侠 <675567585@qq.com>
2023-04-30 23:11:28 +0800

    新库

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
 
 const formats = ['cjs', 'esm'];
 
@@ -13,7 +14,7 @@ export default defineConfig([{
             format: fmt
         }
     }),
-    plugins: [resolve(), typescript({
+    plugins: [commonjs(), resolve(), typescript({
         compilerOptions: {
             outDir: "./dist",
             declaration: false,
