29d888a762f4f60e105f916204fab303f72603e3
youxia <675567585@qq.com>
2023-06-11 00:07:47 +0800

    修正

--- src/core/component/Container.ts
+++ src/core/component/Container.ts
@@ -2,7 +2,4 @@ import { Transform } from "../transform";
 import { BaseContainer } from "./BaseContainer";
 
 export class Container extends BaseContainer {
-    constructor(transform: Transform) {
-        super(transform);
-    }
 }
