0956dc15700e30a09e903eb1262337505e422c53
youxia <675567585@qq.com>
2023-07-11 14:20:53 +0800

    找回文档配置文件

--- src/core/component/Container.ts
+++ src/core/component/Container.ts
@@ -2,7 +2,7 @@ import { Transform } from "../transform";
 import { BaseContainer } from "./BaseContainer";
 
 export class Container extends BaseContainer {
-    constructor(transform: Transform) {
-        super(transform);
+    constructor() {
+        super();
     }
 }
